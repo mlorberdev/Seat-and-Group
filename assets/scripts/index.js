6e75c458d2546cb306ec1170b8ed666caa2ac308
mlorber <87738509+mlorberdev@users.noreply.github.com>
2024-01-09 13:36:49 -0600

    iterative update

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -6,9 +6,16 @@
   const ss = document.getElementById("seats");
   const gg = document.getElementById("groups");
   const ff = document.querySelector("footer"); // footer
-  let pp; // Page, for back button
-  let cc; // Class (period) number
-  let n = 24; // Change to get number of students in class from Google sheet
+  const p1 = ["VIVI","GABRIELA","NEHEMIAS","NESTOR","JORGE","EMELY","DENISSE","ALDO","ALEX","BRAYAN","ANEJIA","KHUI","MARIO","ZABDI","KA'RON","JOHNATHAN","EMMA","MICHAEL","KERIONNA","TANIA","ANDREW","MIKAYLA","TEFERA","EZEKIEL","LAUREN","NATALY","OLIVER","LUIS","NARCY","KEANDRE","CHRIS"];
+  const p2 = ["MARIANO","NEVAEH","JOSEPH","JESUS","MADISON","MARCELA","JOSHUA","KEVIN","MICHAEL","CINDY","ADRIAN","JASMIN","ANGEL","BREANNA","JORDAN","YAZMIN","GENARO","IVIS","DENIS","SELVIN","GENESIS","CRISTIAN","MAYTES"];
+  const p4 = ["KIMBERLY B.","JAMES","MALIAH","FRANCHESKA","KENNIS","KIMBERLY H.","LAUREN","MALAYAH","JUDITH","SIHARA","JUAN","STEVEN","NELIDA","DANIELA","ALBERT","IVAN L.","ANNA","BRANDON","EDWARD","IVAN S.","AMY","JONATHAN","RICHARD","AALIYAH","ERICK","ALEXIS","YAHAIRA","CHRISTOPHER","KADEN","DESTINY","TANIA","LILY","CALEB","ATHZERE","CARLOS","AUBURN"];
+  const p5 = ["KIMBERLIN","SAMANTHA","MAIREL","RODNEY","EMANUEL","HEIRI","MARQUIS","ALEXSANDRA","GIOVANNI","CURTIS","NATHAN","DEONDRICK","DEANNA","ADRIAN","IVAN","CARLOS","ANGEL","CHRISTOPHER","ANTONIO","KIMBERLY","ESTEBAN","JUAN","GENESIS"];
+  const p6 = ["NIKAYLA","ISAIAH","ARMANDO","ARYANNA","GUSTAVO","JOSEPH","ANGIE","SHAKEERA","HILARY","EDEL","TALYAH","ISMAEL","YAHIR","CRISTINA","JOHN","ESTEFANIA","CAMERON","DAI'TON","ALIYAH R.","KAYTREN","DAYANNE","OLIVIA","DE'MARI","DHARION","ALIYIAH N."];
+  const p7 = ["AYA","RAMON","JEZYRETH","SEBASTIAN","MARISELA","JULISSA","NICK","BELLA","ELVIRA","ALEXANDER","LUIS","ARTEMIS","SANIYAH","ISAIAH","DANYA","MELANIE","MYRA","MARICELA","JAKE","JOSHUA","LOANY"];
+  let cc; // Class checked
+  let pp; // Page, for back button (which page you're on: groups or seats)
+  let st; // Students
+  let n; // Number of students in class
 
   // BUTTON CLICK
   document.querySelectorAll("button").forEach(btn => btn.addEventListener("click", function () {
@@ -16,7 +23,7 @@
     switch (this.id) {
       case "btn_s": seats(); break;
       case "btn_g": groups(); break;
-      case "save": window.print();; break;
+      case "save": window.print(); break;
       case "btn_b": back(); break;
       default: break;
     }
@@ -25,7 +32,7 @@
   // RANDOM SEATING CHART
   function seats() {
     pp = "ss"; // Var for which page is open
-    getClass();
+    // getClass();
     let frag = new DocumentFragment();
     for (let i = 0; i < n; i++) { // n students
       let ele = document.createElement("div");
@@ -35,30 +42,56 @@
     ss.appendChild(frag);
     ss.style.display = "grid"; // Show seating
     ii.style.display = "none"; // Hide buttons
-    header.innerHTML = `FRONT / FRENTE`; // Update header
+    header.innerHTML = `FRONT`; // Update header
   }
 
   // RANDOM GROUP ASSIGNMENT
   function groups() {
-    pp = "gg";
-    getClass();
+    pp = "gg"; // Put on groups page
+    let ccs = document.querySelectorAll("input[name=class]");
+    for (let i = 0; i < ccs.length; i++) {
+      if (ccs[i].checked) { cc = i + 1; break };
+    }
+    switch (cc) { // st is student list by period
+      case 1: st = p1; break;
+      case 2: st = p2; break;
+      case 4: st = p4; break;
+      case 5: st = p5; break;
+      case 6: st = p6; break;
+      case 7: st = p7; break;
+    }
+    document.getElementById("class").innerHTML = ``;
+    document.getElementById("stamp").innerHTML = `${new Date().toLocaleDateString()} [ ${cc} ] `; // Timestamp document
+
     let frag = new DocumentFragment();
-    for (let i = 0; i < 6; i++) {
+    let temp = [];
+    for (let i = 0; i < st.length; i++) temp.push(st[i]); // fill temp arr w/students
+    let n = 5; // num of students in each group; changes when num unplaced < 10
+    for (let i = 0; i < 7; i++) {
       let grp = document.createElement("section");
       let ttl = document.createElement("div");
       ttl.innerHTML = i + 1;
       grp.appendChild(ttl);
-      for (let j = 0; j < 4; j++) {
+      switch (temp.length) {
+        case 6: n = 3; break;
+        case 8: n = 4; break;
+        default: break;
+      }
+      for (let j = 0; j < n; j++) {
         let stu = document.createElement("div");
-        stu.innerHTML = Math.random().toFixed(6);
-        grp.appendChild(stu);
+        let s = Math.floor(Math.random() * temp.length); // pick a student from class list, the number
+        stu.innerHTML = temp[s];
+        if (temp.length > 0) {
+          temp.splice(s, 1);
+          grp.appendChild(stu);
+        }
       }
       frag.appendChild(grp);
     }
     gg.appendChild(frag);
     gg.style.display = "grid"; // Show seating
     ii.style.display = "none"; // Hide buttons
-    header.innerHTML = `GROUPS / GRUPOS`; // Update header
+    header.innerHTML = `GROUPS`; // Update header
   }
 
   // BACK BUTTON
@@ -75,13 +108,4 @@
     ii.style.display = "flex";
   }
 
-  // HELPER
-  function getClass() {
-    let ccs = document.querySelectorAll("input[name=class]");
-    for (let i = 0; i < ccs.length; i++) {
-      if (ccs[i].checked) cc = i + 1;
-    }
-    document.getElementById("class").innerHTML = ``;
-    document.getElementById("stamp").innerHTML = `${new Date().toLocaleDateString()} [ ${cc} ] `
-  }
 })();
